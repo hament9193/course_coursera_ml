03bb9de048bc265cbb7ca4a567ad94c1aa1d50d4
hamentchoudhary <hament9193@gmail.com>
2017-11-28 16:33:45 +0530

    complete

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -21,9 +21,10 @@ grad = zeros(size(theta));
 %
 
 
-
+%this is the cost value
 J = (1 / m) * sum( -y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta)) );
 
+%this is the gradient vector.
 grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -20,8 +20,10 @@ grad = zeros(size(theta));
 
 J = ( (1 / m) * sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta))) ) + (lambda/(2*m))*sum(theta(2:length(theta)).*theta(2:length(theta))) ;
 
+%this is the gradient vector for different thetas.
 grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
 
+% adding regularization to all the grad except the bias weight
 grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
 
 
